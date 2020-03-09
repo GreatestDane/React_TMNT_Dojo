7b23498384dc5feec24ed21550f40b315f985433
Dane M Goodman <DaneGoodman85@gmail.com>
2020-03-08 17:17:41 -0700

    Just added the header and logo, haven't styled yet

---
+++ tmnt-dojo/src/components/elements/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'; 
+
+const Header = () => {
+    return (
+        <div className="tmnt-header">
+            <div className="tmnt-header-content">
+                <img className="tmnt-logo" src="./images/dojo_logo.jpg" alt="TMNT Dojo Logo" />
+            </div>
+        </div>
+    )
+}
