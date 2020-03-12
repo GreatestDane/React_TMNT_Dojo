91ef54425b0974d0cbab5b6ccd465aff029b5173
Dane M Goodman <DaneGoodman85@gmail.com>
2020-03-11 23:36:25 -0700

    I've finally styled the homepage to where I want it and it looks GOOD!

--- tmnt-dojo/src/components/elements/Fighters/Fighters.js
+++ tmnt-dojo/src/components/elements/Fighters/Fighters.js
@@ -3,7 +3,8 @@ import './Fighters.css';
 
 class Fighters extends Component {
     state = {
-        turtles: [<img src='./images/Don.jpg' className='images' alt="Donatello" />, <img src='./images/Leo.jpg' className='images' alt="Leonardo" />, <img src='./images/Mikey.jpg' className='images' alt="Michaelangelo" />, <img src='./images/Raph.jpg' className='images' alt="Raphael" />]
+        turtles: [<img src='./images/Don.jpg' className='images' alt="Donatello" />, <img src='./images/Leo.jpg' className='images' alt="Leonardo" />, <img src='./images/Mikey.jpg' className='images' alt="Michaelangelo" />, <img src='./images/Raph.jpg' className='images' alt="Raphael" />],
+        names: [<div className="names" id="don">Donatello</div>, <div className="names" id="leo">Leonardo</div>, <div className="names" id="mikey">Michaelangelo</div>, <div className="names" id="raph">Raphael</div>]
     }
 
 
@@ -18,14 +19,24 @@ class Fighters extends Component {
         return gridElements;
     }
 
+    renderNames = () => {
+        const gridElements = this.state.names.map((element, i) => {
+            return (
+                <div key={i} className="tmnt-grid-names">
+                    {element}
+                </div>
+            )
+        })
+        return gridElements;
+    }
+
     render() {
         return (
-            <div className="tmnt-grid">
-                
+            <div className="tmnt-grid">    
                 <div className="tmnt-grid-content">
                     {this.renderElements()}
+                    {this.renderNames()}
                 </div>
-                {/* <button onClick={ this.renderElements() }>Render Turtles!</button> */}
             </div>
         )
     }
