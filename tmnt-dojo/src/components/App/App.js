0f549b04fbc0e0760285f8086302e327ab78c27b
Dane M Goodman <DaneGoodman85@gmail.com>
2020-03-12 23:58:44 -0700

    Installed react-router-dom, gonna give this another go

--- tmnt-dojo/src/components/App/App.js
+++ tmnt-dojo/src/components/App/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from "../elements/Header/Header.js";
-import Home from '../Home/Home.js';
+import Header from "../elements/Header/Header";
+import Home from '../Home/Home';
+import Mikey from '../elements/Mikey/Mikey';
 
 
 function App() {
--- tmnt-dojo/src/components/elements/Fighters/Fighters.js
+++ tmnt-dojo/src/components/elements/Fighters/Fighters.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Fighters.css';
 
 class Fighters extends Component {
